80283ecc028765be57933f19584c01982ecbaebf
AHAPX <anarchy.b@gmail.com>
2018-07-12 14:26:33 +0700

    improved setup for pypi

--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
+import setuptools
 from distutils.core import setup
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
 
 setup(
-  name = 'bxinth',
-  packages = ['bxinth'],
+  name='bxinth',
+  packages=['bxinth'],
   package_dir={'bxinth': 'src/'},
-  version = '0.1',
-  description = 'Python lib for bx.in.th market',
-  author = 'AHAPX',
-  author_email = 'anarchy.b@gmail.com',
-  url = 'https://github.com/AHAPX/bxin',
-  download_url = 'https://github.com/AHAPX/bxin/archive/0.1.tar.gz',
+  version='0.1.1',
+  description='Python lib for bx.in.th market',
+  long_description=long_description,
+  license='MIT',
+  author='AHAPX',
+  author_email='anarchy.b@gmail.com',
+  url='https://github.com/AHAPX/bxin',
+  download_url='https://github.com/AHAPX/bxin/archive/0.1.1.tar.gz',
   install_requires=[
     'pyotp==2.2.6',
     'requests==2.18.4'
   ],
-  keywords = ['crypto', 'bx', 'bitcoin'],
-  classifiers = [],
+  keywords=['crypto', 'bx', 'bitcoin'],
+  classifiers=[],
 )
